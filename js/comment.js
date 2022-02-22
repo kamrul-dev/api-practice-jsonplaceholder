1a1aeae04dc022265354d325b051cf01077e4455
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-22 13:34:21 +0600

    Display comment from the API

--- js/comment.js
+++ js/comment.js
@@ -5,3 +5,19 @@ function loadComments() {
         .then(res => res.json())
         .then(data => displayComment(data));
 }
+loadComments();
+
+// display data
+function displayComment(comments) {
+    const CommentsContainer = document.getElementById('comments');
+    for (const comment of comments) {
+        const div = document.createElement('div');
+        div.classList.add('comment')
+        div.innerHTML = `
+        <h3>name : ${comment.name}</h3>
+        <h4>email : ${comment.email}</h4>
+        <P>body : ${comment.body}</p>
+        `
+        CommentsContainer.appendChild(div);
+    }
+}
