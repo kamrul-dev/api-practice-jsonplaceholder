3a192180842ba6c32f38d174acff29e93c37c4b4
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-22 14:08:49 +0600

    load photos and display photos on uI

--- js/photos.js
+++ js/photos.js
@@ -6,3 +6,16 @@ function loadPhotos(){
 }
 loadPhotos();
 
+// display photos on UI
+function displayPhotos(photos){
+    const photosContainer = document.getElementById('photo');
+    for(const photo of photos){
+        const div = document.createElement('div');
+        div.classList.add('photos');
+        div.innerHTML = `
+        <h4>${photo.title}</h4>
+        <img src= "${photo.url}">
+        `
+        photosContainer.appendChild(div);
+    }
+}
